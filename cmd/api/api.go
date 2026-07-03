9b94540216b5130b916c04aa0b92c8f68c36335e
Shreeram Mutukundu <shreerammutukundu.2003@gmail.com>
2026-07-03 15:25:34 +0530

    feat(api): enhance middleware stack with request ID, client IP, recovery, and timeout

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -19,7 +19,15 @@ type config struct{
 
 func( app *application) mount()http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.ClientIPFromRemoteAddr) // pick one ClientIPFrom* based on your infra, see below
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+
+	r.Use(middleware.Timeout(60 * time.Second))
+	
 	r.Route("/v1",func(r chi.Router){
 		r.Get("/health", app.healthCheckHandler)
 	})
